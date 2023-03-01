d1090f922570844de5f11fdc0f2b5d3d3ffa0f98
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-01 17:52:37 +0000

    More updates to HTML and Javascript

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let firstCard = 10
 let secondCard = 7
-let sum = firstCard + secondCard
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#card-el")
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🫤")
-    isAlive = true
-} else if (sum === 21){
-    console.log("Woohoo! You've got Blackjack!")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game")
-    isAlive = false
+
+function startGame(){
+    renderGame()
 }
 
-console.log(isAlive)
+function renderGame (){
+    cardsEl.textContent = "Your cards: " + cards[0] + " & " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    
+    if (sum <= 20) {
+        message= "Do you want to draw a new card?"
+        isAlive = true
+    } else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game"
+        isAlive = false
+    }
+        messageEl.textContent = message
+
+}
+
+
+function drawCard(){
+    let newCard = 1
+    sum += newCard
+    renderGame()
+}
