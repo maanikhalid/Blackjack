891066d46b3f488a1d486db5b765776f5290715e
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-01 17:07:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,18 @@
-// Variables set
 let firstCard = 10
-let secondCard = 9
-let playerTotal = firstCard + secondCard;
+let secondCard = 7
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
-//GET DOM elements 
-const myPlayerCardsElement = document.getElementById("playerCards");
-const myPlayerFirstCardElement = document.getElementById("firstCard");
-const myPlayerSecondCardElement = document.getElementById("secondCard");
-const myPlayerTotalElement = document.getElementById("playerTotalCards");
-
-const aiCardsElement = document.getElementById("aiCards");
-const aiTotalElement = document.getElementById("aiTotalCards");
-
-//DOM messages
-const winner = document.getElementById("btn-winner");
-const loser = document.getElementById("btn-loser");
-const draw = document.getElementById("btn-draw");
-
-const winnerAI = document.getElementById("btn-winner-ai");
-const loserAI = document.getElementById("btn-loser-ai");
-const drawAI = document.getElementById("btn-draw-ai");
-
-
-// Check player cards
-myPlayerFirstCardElement.innerText = firstCard;
-myPlayerSecondCardElement.innerText = secondCard;
-
-// Display total
-myPlayerTotalElement.innerText = playerTotal;
-
-
-// Display status
-winner.style.display ="none";
-loser.style.display ="none";
-draw.style.display ="none";
-winnerAI.style.display ="none";
-loserAI.style.display ="none";
-drawAI.style.display ="none";
-
-if (playerTotal > 21) {
-    winner.style.display ="none";
-    loser.style.display ="block";
-    draw.style.display ="none";
-    winnerAI.style.display ="block";
-    loserAI.style.display ="none";
-    drawAI.style.display ="none";
-} else if (playerTotal < 21) {
-    winner.style.display ="block";
-    loser.style.display ="none";
-    draw.style.display ="none";
-    winnerAI.style.display ="none";
-    loserAI.style.display ="block";
-    drawAI.style.display ="none";
-}else {
-    winner.style.display ="none";
-    loser.style.display ="none";
-    draw.style.display ="nblock";
-    winnerAI.style.display ="none";
-    loserAI.style.display ="none";
-    drawAI.style.display ="block";
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🫤")
+    isAlive = true
+} else if (sum === 21){
+    console.log("Woohoo! You've got Blackjack!")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game")
+    isAlive = false
 }
 
-
-// Generate a random number 
-function drawCard(){
-
-}
-
-
+console.log(isAlive)
