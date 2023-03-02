29e146afd2c06feaa374c8abd2166b93b164597d
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-02 18:04:36 +0000

    Update to script - Added AI opponent

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let firstCard = getRandomCard()
 let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
-let aiCardsDraw = getRandomCard()
+let aiCardsDraw = []
+let aiTotalSumm = []
 let sum = firstCard + secondCard 
+let sumAI = []
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageAi = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#card-el")
@@ -26,7 +29,7 @@ function renderGame (){
     cardsEl.textContent = "Your cards: "
     aicardsEl.textContent = "AI cards: " 
     
-    for (i = 1 ; i < )
+   
     
     for (i = 0 ; i < cards.length ; i++)
         {
@@ -35,24 +38,58 @@ function renderGame (){
         }
 
     sumEl.textContent = "Sum: " + sum
+
     
     if (sum <= 20) {
-        message= "Do you want to draw a new card?"
+        message= "Draw a new card?"
+        messageAi = "The AI drew these cards"
         isAlive = true
     } else if (sum === 21){
         message = "You've got Blackjack!"
         hasBlackJack = true
+        messageAi = "The AI drew these cards"
     } else {
         message = "You're out of the game"
+        messageAi = "The AI wins the game"
         isAlive = false
     }
         messageEl.textContent = message
+        aimessageEl.textContent = messageAi
+    
+    
+    // AI first draw
+    for  (i = 0 ;  i < 20 ; i++) {
+        
+        if (sumAI.length < 3) {
+    let aiCardDraw = getRandomCard()
+    sumAI += aiCardDraw
+    
+    aiCardsDraw.push(aiCardDraw) 
+    console.log(aiCardsDraw) 
+            
+            } else {
+                console.log("This is the end")
+            }
+    
+    }
+    
+        aicardsEl.textContent += (aiCardsDraw) 
+    
+
+    
+    aisumEl.textContent = aiCardsDraw.reduce((pv, cv) => pv + cv, 0);
 
 }
+
 
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random()*10 +1) 
     return randomNumber;
+}
+
+function aiDrawCard(){
+
+    
 }
 
 function drawCard(){
