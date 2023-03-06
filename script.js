8ed5e6108f0236d9aea4ebe87ffd45c9f69a3deb
Khalid <52281551+maanikhalid@users.noreply.github.com>
2023-03-06 12:00:42 +0000

    Updates

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let aiCardsDraw = []
-let aiTotalSumm = []
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let sumAI = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageAi = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#card-el")
-let aimessageEl = document.getElementById("aimessage-el")
-let aisumEl = document.querySelector("#aisum-el")
-let aicardsEl = document.querySelector("#aicard-el")
 
-
-function  getRandomCard() {
-    console.log("Hello")
-}
-
-function startGame(){
-    renderGame()
-}
+console.log (cards)
+console.log (sum)
 
 function renderGame (){
     cardsEl.textContent = "Your cards: "
-    aicardsEl.textContent = "AI cards: " 
-    
-   
     
     for (i = 0 ; i < cards.length ; i++)
         {
@@ -42,54 +26,40 @@ function renderGame (){
     
     if (sum <= 20) {
         message= "Draw a new card?"
-        messageAi = "The AI drew these cards"
         isAlive = true
     } else if (sum === 21){
         message = "You've got Blackjack!"
         hasBlackJack = true
-        messageAi = "The AI drew these cards"
     } else {
         message = "You're out of the game"
         messageAi = "The AI wins the game"
         isAlive = false
     }
         messageEl.textContent = message
-        aimessageEl.textContent = messageAi
-    
-    
-    // AI first draw
-    for  (i = 0 ;  i < 20 ; i++) {
-        
-        if (sumAI.length < 3) {
-    let aiCardDraw = getRandomCard()
-    sumAI += aiCardDraw
-    
-    aiCardsDraw.push(aiCardDraw) 
-    console.log(aiCardsDraw) 
-            
-            } else {
-                console.log("This is the end")
-            }
-    
-    }
-    
-        aicardsEl.textContent += (aiCardsDraw) 
-    
-
-    
-    aisumEl.textContent = aiCardsDraw.reduce((pv, cv) => pv + cv, 0);
-
 }
+
 
 
 function getRandomCard(){
-    let randomNumber = Math.floor(Math.random()*10 +1) 
-    return randomNumber;
+    let randomNumber = Math.floor(Math.random()*13 +1) 
+    if (randomNumber > 10){
+        return 10 
+    } else if (randomNumber === 1){
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
-function aiDrawCard(){
-
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+        
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     
+    renderGame()
 }
 
 function drawCard(){
